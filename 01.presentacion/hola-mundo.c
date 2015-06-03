8cb0d915770ded85c51bcac59bf607192e05380a
jaime <jvidalgz@gmail.com>
2015-06-03 20:51:48 -0300

    crea ejemplo de uso de printf, con un simple 'hola mundo'

---
+++ 01.presentacion/hola-mundo.c
@@ -0,0 +1,17 @@
+/*
+Ejemplo de programa en c, que muestra el uso de la 
+libreria estandar por medio de la funcion prinft()
+@jaime vidal
+
+*/
+
+ int main()
+{
+	/* printf imprime por pantalla el texto contenido dentro 
+	de los parentesis.'\n' genera un salto de línea 
+	(una nueva línea) */
+	printf("Hola mundo\n");
+
+	return 0;
+}
+
