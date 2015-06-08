6d9e42806a91a7aa5a437fdb89b04f43e593fc90
jaime <jvidalgz@gmail.com>
2015-06-08 11:13:35 -0300

    agrega una tercera variable, está en formato exponencial

--- 02.tipos-datos/float-ejemplos-simples.c
+++ 02.tipos-datos/float-ejemplos-simples.c
@@ -7,16 +7,19 @@ luego muestrasus contenidos por pantalla
 int main(){
 
 	// se declaran las variables
-	float var1,var2;
+	float var1,var2,var3;
 
 	// se les asignan valores
 	var1=2.3;
-	var2= 10.456789;
+	var2=10.456789;
+	var3=22.442e2;
 
 	// imprime var1
 	printf("var1: %f\n",var1);
 	// imprime var2
 	printf("var2: %f\n",var2);
+	// imprime var3
+	printf("var3: %f\n",var3);
 
 	return 0;
 }
