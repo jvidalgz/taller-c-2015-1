59a09324451c485c8cfaf735427efe3d168cfef8
jaime <jvidalgz@gmail.com>
2015-06-08 11:09:02 -0300

    agrega código que muestra el uso simple de variables del tipo float.

---
+++ 02.tipos-datos/float-ejemplos-simples.c
@@ -0,0 +1,22 @@
+/**
+Programa muestra la declaracion y asignacion de valores a dos variables del tipo float, 
+luego muestrasus contenidos por pantalla
+**/
+#include <stdio.h>
+
+int main(){
+
+	// se declaran las variables
+	float var1,var2;
+
+	// se les asignan valores
+	var1=2.3;
+	var2= 10.456789;
+
+	// imprime var1
+	printf("var1: %f\n",var1);
+	// imprime var2
+	printf("var2: %f\n",var2);
+
+	return 0;
+}
