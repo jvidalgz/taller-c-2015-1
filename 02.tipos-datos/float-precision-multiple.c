3bb1e49d8352852ac80b2d149f858d47b1f72a36
jaime <jvidalgz@gmail.com>
2015-06-08 17:58:50 -0300

    agrega múltiples ejemplos de precisión para un tipo de dato float

---
+++ 02.tipos-datos/float-precision-multiple.c
@@ -0,0 +1,25 @@
+/*
+  programa que muestra el uso de distintos formatos de precision para imprimir una
+  variable del tipo float
+*/
+#include <stdio.h>
+ 
+int main() {
+
+  // se declara variable
+  float x = 3.141592;
+  
+  // se muestran distintos tipos de formatos de precision para la variable 'x'
+  printf("Imprimiendo 3.141592 usando formato %%f    \t  %f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%2.1f \t  %2.1f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%3.2f \t  %3.2f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%4.3f \t  %4.3f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%5.4f \t  %5.4f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%6.5f \t  %6.5f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%09.3f    %09.3f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%-09.3f       %-09.3f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%9.3f     %9.3f\n", x);
+  printf("Imprimiendo 3.141592 usando formato %%-9.3f    %-9.3f\n", x);
+  
+  return 0;
+}
